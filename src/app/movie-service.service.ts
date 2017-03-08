9b66c99902a89d5ab812417bdc664fb188744225
Mike <mike@miketruax.com>
2017-03-08 10:07:57 -0600

    Adds multiple call funcitonality

--- src/app/movie-service.service.ts
+++ src/app/movie-service.service.ts
@@ -4,25 +4,39 @@ import {Http} from "@angular/http";
 @Injectable()
 export class MovieService {
   constructor(private http: Http) { }
-  private returnData(r:Array<Object>){
-
+  private returnData(results:Array<Object>){
+      let actorsInCommon = results[0]['Actors'].split(', ');
+      console.log(actorsInCommon);
+      results.forEach((e, i, results) => {
+        let actors = e['Actors'].split(', ');
+        actorsInCommon.forEach((e,i, actorsInCommon)=>{
+          if(actors.indexOf(e) === -1){
+            actorsInCommon.splice(i, 1);
+          }
+        });
+      });
+      console.log(actorsInCommon);
   }
     getMovies(movies: Array<Object>){
       let x = movies.length;
-      let results = [{}];
+      let results = [];
 
       movies.forEach((e, i, array) =>{
-        this.http.get('http://www.omdbapi.com/?t='+array[i]['movie']+'&y=&plot=short&r=json')
-          // .map(res => res.json())
-          .subscribe(response => {
-            console.log(response, 'http://www.omdbapi.com/?t='+array[i]['movie']+'&y=&plot=short&r=json')
-            x--;
-            results.push(response);
-            if(x<=0){
-              this.returnData(results);
-            }
-          })
-          })
+        if(e['movie']) {
+          this.http.get('http://www.omdbapi.com/?t=' + array[i]['movie'] + '&y=&plot=short&r=json')
+            .map(res => res.json())
+            .subscribe(response => {
+              x--;
+              results.push(response);
+              if (x <= 0) {
+                return this.returnData(results);
+              }
+            })
+        } else{
+          x--;
+        }
+        })
+
       }
 
 
